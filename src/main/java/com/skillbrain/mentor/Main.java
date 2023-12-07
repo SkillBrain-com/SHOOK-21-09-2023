a546c39e04f75e8793d736075d1e131de81dcad4
andreisoare21 <andrei.daniel.soare@gmail.com>
2023-12-07 19:56:19 +0200

    slenium-first chapter

---
+++ src/main/java/com/skillbrain/mentor/Main.java
@@ -0,0 +1,20 @@
+package com.skillbrain.mentor;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/");
+
+
+        driver.quit();
+    }
+}
